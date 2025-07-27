a694c06f45ef8674fa5be8dc408b18c15f96111e
Your Name <you@example.com>
2025-07-28 02:07:47 +0600

    chat is added

---
+++ src/Components/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Chat = () => {
+    return (
+        <div>
+                this is chat section
+            <h1>Chat Component</h1>
+            <p>This is chat section</p>
+        </div>
+    );
+};
+
+export default Chat;
