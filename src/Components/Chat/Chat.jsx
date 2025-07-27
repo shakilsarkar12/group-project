23275acbf26190679b78720ccfa7a0632001a0df
Your Name <you@example.com>
2025-07-28 02:11:22 +0600

    delete the data

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Chat = () => {
     return (
         <div>
-                this is chat section
-            <h1>Chat Component</h1>
-            <p>This is chat section</p>
+            
         </div>
     );
 };
