4d2eee4b302d3992831ebca466e3de555c87a209
yeasin-islam <info.yeasinislam@gmail.com>
2025-07-28 00:58:10 +0600

    Text Added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,10 +7,8 @@ const Navbar = () => {
         <div className='flex items-center justify-between px-16 h-16'>
             <h1 className='text-2xl font-bold'>Navbar Dorkar Nai</h1>
             <ul className='navlink flex gap-4 text-xl font-semibold'>
-                <NavLink to='/'>Home Nai</NavLink>
-                <NavLink to='/about'>About Me Md Yeasin Islam</NavLink>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/'>Home Ache</NavLink>
+                <NavLink to='/about'>About Janar dorka nai</NavLink>
                 <NavLink to='/contact'>Contact</NavLink>
             </ul>
         </div>
