c826303cf0a9bf04c9ac0dfc25c4e956c300cad3
Your Name <you@example.com>
2025-07-28 01:21:33 +0600

    my name is added here

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className='w-full h-[calc(100vh-64px)] flex items-center justify-center'>
             <h1 className='text-5xl'>Home Edited</h1>
+            <h1>Tanmay Islam</h1>
         </div>
     );
 };
