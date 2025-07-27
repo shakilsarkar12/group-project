a17169224fc68614bee45f621d817f3e58b79eab
yeasin-islam <info.yeasinislam@gmail.com>
2025-07-28 00:51:46 +0600

    add some text in Navber

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import '../../../src/App.css'
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between px-16 h-16'>
-            <h1 className='text-2xl font-bold'>Navbar</h1>
+            <h1 className='text-2xl font-bold'>Navbar Dorkar Nai</h1>
             <ul className='navlink flex gap-4 text-xl font-semibold'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/'>Home Nai</NavLink>
+                <NavLink to='/about'>About Me Md Yeasin Islam</NavLink>
             </ul>
         </div>
     );
